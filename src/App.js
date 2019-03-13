1a34282e2f557368da9abacac312341a97e082a7
goransle <goransle@gmail.com>
2019-03-13 15:38:15 +0100

    småfiks

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import History from './components/History'
 import localForage from 'localforage'
 
 const today = Moment();
-var defaultData = { bedtime: "23:00", waketime: '07:25', rating: 5 };
+var defaultData = { bedtime: "23:00", waketime: '07:25', rating: 5, troubleSleeping: 0 };
 
 class App extends Component {
   constructor(props) {
@@ -22,25 +22,27 @@ class App extends Component {
       selectedDay: today.format('L'),
       logData: {}
     }
-    this.onChange = this.onChange.bind(this);
   }
 
-
   componentDidMount() {
-    this.setState({ logData: defaultData });
     this.onChange(this.state.selectedDay)
   }
 
-  onChange(date) {
-    console.log(date)
+  componentDidUpdate(prevState){
+    console.log(prevState)
+  }
+
+  onChange = (date) => {
     this.setState({ selectedDay: date });
     //setting default data
-    this.setState({ logData: defaultData });
+    // this.setState({ logData: defaultData });
     //gets stuff from storage
     localForage.getItem(date).then((item) => {
       if (item) {
         this.setState({ logData: item });
       }
+      else
+        this.setState({ logData: defaultData });
     })
   }
 
@@ -56,12 +58,10 @@ class App extends Component {
           <Route exact path="/" render={props => (
             <React.Fragment>
               <Days today={this.state.today} selected={this.state.selectedDay} dateChange={this.onChange} />
-              <Logging day={this.state.selectedDay} data={this.state.logData} inputHandler={this.inputHandler} />
+              <Logging day={this.state.selectedDay} data={this.state.logData} inputHandler={this.inputHandler} state={props} />
             </React.Fragment>
           )} />
-        <Route path="/history" render={props =>(
-          <History/>
-        )} />
+        <Route path="/history" render={props =>(<History/>)} />
         </div>
       </Router>
     );
