81287656b6761c730023ae1dd3029ff9eb3e31a7
goransle <goransle@gmail.com>
2019-03-27 09:44:21 +0100

    Separeted out

---
+++ src/components/ClockInput.js
@@ -0,0 +1,122 @@
+import React, {useRef } from "react";
+import Moment from "moment";
+import {
+  CircularInput,
+  CircularTrack,
+  CircularProgress,
+  CircularThumb,
+  useCircularInputContext,
+  useCircularDrag
+} from "react-circular-input";
+
+export default function ClockInput(props) {
+  const CenterText = props => {
+    var diff = () => {
+      if (props.end > props.start) {
+        return `${Moment.duration({
+          hours: props.end.hours() - props.start.hours()
+        }).humanize()} ${Moment.duration({
+          minutes: props.end.minutes() - props.start.minutes()
+        }).humanize()}`;
+      } else {
+        return `${Moment.duration({
+          hours: 24 + props.end.hours() - props.start.hours()
+        }).humanize()} ${Moment.duration({
+          minutes: props.end.minutes() - props.start.minutes()
+        }).humanize()}`;
+      }
+    };
+    return (
+      <text x={100} y={100} textAnchor="middle" dy="0.3em" fontWeight="bold">
+        {diff()}
+      </text>
+    );
+  };
+  const CustomProgress = props => {
+    const { value, radius, center } = useCircularInputContext();
+    const innerCircumference = Math.PI * 360;
+
+    var duration = () => {
+      if (props.start > props.end && props.start < 1) {
+        return 1 - (props.end + (1 - props.start)) / 1.8;
+      } else if (props.start > props.end && props.start < 2) {
+        return 1 - (props.end + (2 - props.start)) / 1.8;
+      }
+      return 1 - (props.end - props.start) / 1.8;
+    };
+    var degrees = () => {
+      if (props.start < 1) {
+        return 360 * props.start - 90;
+      }
+      return 360 * props.start + 270;
+    };
+    return (
+      <CircularTrack
+        {...props}
+        stroke={"#3D99FF"}
+        strokeDasharray={innerCircumference}
+        strokeDashoffset={innerCircumference * duration()}
+        transform={`rotate(${degrees()} ${center.x} ${center.y})`}
+      />
+    );
+  };
+  const CustomThumb = props => {
+    const {
+      getPointFromValue,
+      isFocused,
+      setFocused
+    } = useCircularInputContext();
+    const { x, y } = getPointFromValue();
+
+    const ref = useRef(null);
+    const { isDragging } = useCircularDrag(ref)
+
+    return (
+      <circle r={isFocused || isDragging ? 23 : 20} {...props} ref={ref} cx={x} cy={y} />
+    );
+  };
+  return (
+    <React.Fragment>
+      <CircularInput
+        value={
+          Moment(props.waketime, "hh:mm").hours() / 12 +
+          Moment(props.waketime, "hh:mm").minutes() / 720
+        }
+        classID={"main"}
+        style={{ position: "absolute" }}
+        onChange={props.handleBedtime}
+      >
+        <CircularTrack />
+        <CustomProgress
+          end={
+            Moment(props.waketime, "hh:mm").hours() / 12 +
+            Moment(props.waketime, "hh:mm").minutes() / 720
+          }
+          start={
+            Moment(props.bedtime, "hh:mm").hours() / 12 +
+            Moment(props.bedtime, "hh:mm").minutes() / 720
+          }
+        />
+        <CustomThumb
+          style={{zIndex:100}}
+        />
+        <CenterText
+          end={Moment(props.waketime, "hh:mm")}
+          start={Moment(props.bedtime, "hh:mm")}
+        />
+        <CircularInput
+        value={
+          Moment(props.bedtime, "hh:mm").hours() / 12 +
+          Moment(props.bedtime, "hh:mm").minutes() / 720
+        }
+        style={{ position: "absolute" }}
+        onChange={props.handleWaketime}
+      >
+        <CustomThumb
+          style={{zIndex:0}}
+        />
+      </CircularInput>
+      </CircularInput>
+    </React.Fragment>
+  );
+}
--- src/components/Logging.js
+++ src/components/Logging.js
@@ -1,15 +1,6 @@
 import React, { Component, useRef } from "react";
 import TimePicker from "react-time-picker";
-import Moment from "moment";
-
-import {
-  CircularInput,
-  CircularTrack,
-  CircularProgress,
-  CircularThumb,
-  useCircularInputContext,
-  useCircularDrag
-} from "react-circular-input";
+import ClockInput from "./ClockInput";
 
 class Logging extends Component {
   constructor(props) {
@@ -75,62 +66,6 @@ class Logging extends Component {
   render() {
     var { data } = this.props;
 
-    const CenterText = (props) => {
-      var diff = () => {
-      if(props.end > props.start){
-        return `${Moment.duration({hours: props.end.hours() - props.start.hours()}).humanize()} ${Moment.duration({minutes: props.end.minutes() - props.start.minutes()}).humanize()}`;
-      }
-      else{
-        return `${Moment.duration({hours: 24 + props.end.hours() - props.start.hours()}).humanize()} ${Moment.duration({minutes: props.end.minutes() - props.start.minutes()}).humanize()}`;
-      }
-    }
-      return (
-        <text x={100} y={100} textAnchor="middle" dy="0.3em" fontWeight="bold">
-          {diff()}
-        </text>
-      );
-    };
-    const CustomProgress = (props) => {
-      const {value, radius, center } = useCircularInputContext()
-      const innerCircumference = (Math.PI * 360)
-
-      var duration = () =>{ 
-        if(props.start > props.end && props.start < 1 ){
-          return  1 - (props.end + (1 - props.start)) / 1.8
-        }
-        else if(props.start > props.end && props.start < 2 ){
-          return  1 - (props.end + (2 - props.start)) / 1.8
-        }
-        return 1 - (props.end - props.start) / 1.8
-      }
-      console.log(duration())
-      var degrees = () =>{ 
-        if(props.start < 1){
-          return (360 * props.start) - 90
-        }
-        return (360 * props.start) + 270
-      }
-      console.log(degrees())
-      return (
-        <CircularTrack
-          {... props}
-          stroke= {'#3D99FF'}
-          strokeDasharray={innerCircumference}
-          strokeDashoffset={innerCircumference * duration()}
-          transform={`rotate(${(degrees())} ${center.x} ${center.y})`}
-        />
-      )
-    };
-    const CustomThumb = () => {
-      const { getPointFromValue } = useCircularInputContext();
-      const { x, y } = getPointFromValue();
-
-      const ref = useRef(CircularInput);
-      useCircularDrag(ref);
-
-      return <circle ref={ref} cx={x} cy={y} r={20} />;
-    };
-
     return (
       <form onSubmit={this.onSubmit}>
         <div className="form-group">
@@ -156,38 +91,8 @@ class Logging extends Component {
             />
           </div>
         </div>
-        <div className="form-group">
-          <CircularInput
-            value={
-              Moment(this.state.waketime, "hh:mm").hours() / 12 +
-              Moment(this.state.waketime, "hh:mm").minutes() / 720
-            }
-            onChange={this.handleCircle}
-          >
-            <CircularTrack />
-            <CustomProgress end={
-              Moment(this.state.waketime, "hh:mm").hours() / 12 +
-              Moment(this.state.waketime, "hh:mm").minutes() / 720
-            } start={
-              Moment(this.state.bedtime, "hh:mm").hours() / 12 +
-              Moment(this.state.bedtime, "hh:mm").minutes() / 720
-            } />
-            <CircularThumb/>
-            <CircularInput
-              value={
-                Moment(this.state.bedtime, "hh:mm").hours() / 12 +
-                Moment(this.state.bedtime, "hh:mm").minutes() / 720
-              }
-              onChange={this.handleCustom}
-            >
-              <CustomThumb/>
-            </CircularInput>
-            <CenterText end={
-              Moment(this.state.waketime, "hh:mm")
-            } start={
-              Moment(this.state.bedtime, "hh:mm")
-            } />
-          </CircularInput>
+        <div className="form-group" style={{width: "50%", marginBottom:"250px"}}>
+          <ClockInput bedtime={this.state.bedtime} waketime={this.state.waketime} handleBedtime={this.handleCircle} handleWaketime={this.handleCustom}/>
         </div>
         <div className="form-group">
           <input
