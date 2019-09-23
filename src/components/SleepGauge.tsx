a2af1c40ac9981f855fd0504a40b63eaa02472d1
Gøran A. Slettemark <goransle@gmail.com>
2019-09-23 21:23:15 +0200

    converted to ts

--- src/components/SleepGauge.tsx
+++ src/components/SleepGauge.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Highcharts from 'highcharts'
+import Highcharts, { Series, Chart, ChartOptions, LegendOptions, TitleOptions, XAxisOptions, SeriesOptions, YAxisOptions, SeriesSolidgaugeOptions, PlotGaugeOptions, SeriesGaugeOptions, AxisPlotBandsOptions, Options } from 'highcharts'
 import HC_more from 'highcharts/highcharts-more'
 import HC_solidGauge from 'highcharts/modules/solid-gauge'
 import HC_bulletGraph from 'highcharts/modules/bullet'
@@ -31,14 +31,25 @@ export default function SleepGauge(props) {
         color: '#2ECC40'
     }]
 
-    var chart = {
+    const chart = {
         inverted: true,
         marginLeft: 0,
         type: 'bullet',
         useHTML: true
     };
 
-    var durationGauge = {
+    interface GaugeChart {
+        chart: ChartOptions,
+        legend: LegendOptions,
+        title:TitleOptions,
+        xAxis:XAxisOptions,
+        yAxis:YAxisOptions,
+        series:SeriesSolidgaugeOptions,
+
+
+    }
+
+    var durationGauge:Options = {
         chart: chart,
         legend: {
             enabled: false
@@ -59,30 +70,27 @@ export default function SleepGauge(props) {
             max: 100
         },
         series: [{
+            type:'bullet',
             data: [{
                 target: 101,
                 y: Number(props.durationScore)
             }],
-            color: 'blue'
+            color: 'blue',
         }],
         tooltip: {
             pointFormat: '<b>{point.y}</b> (of 100)'
         },
         plotOptions: {
             series: {
-                pointPadding: 0.25,
                 borderWidth: 0,
-                color: '#000',
-                targetOptions: {
-                    width: '200%'
-                }
+                color: '#000'
             }
         },
         credits: {
             enabled: false
         },
     };
-    var ratingGauge = {
+    var ratingGauge:Options = {
         chart: chart,
         legend: {
             enabled: false
@@ -103,6 +111,7 @@ export default function SleepGauge(props) {
             max: 100
         },
         series: [{
+            type:'bullet',
             data: [{
                 target: 101,
                 y: Number(props.ratingScore)
@@ -114,12 +123,8 @@ export default function SleepGauge(props) {
         },
         plotOptions: {
             series: {
-                pointPadding: 0.25,
                 borderWidth: 0,
-                color: '#000',
-                targetOptions: {
-                    width: '200%'
-                }
+                color: '#000'
             }
         },
         credits: {
@@ -127,7 +132,7 @@ export default function SleepGauge(props) {
         }
     };
 
-    var consistencyGauge = {
+    var consistencyGauge:Options = {
         chart: chart,
         legend: {
             enabled: false
@@ -148,6 +153,7 @@ export default function SleepGauge(props) {
             max: 100
         },
         series: [{
+            type: 'bullet',
             data: [{
                 target: 101,
                 y: Number(props.consistencyScore)
@@ -159,12 +165,8 @@ export default function SleepGauge(props) {
         },
         plotOptions: {
             series: {
-                pointPadding: 0.25,
                 borderWidth: 0,
-                color: '#000',
-                targetOptions: {
-                    width: '200%'
-                }
+                color: '#000'
             }
         },
         credits: {
